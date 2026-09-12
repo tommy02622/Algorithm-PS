d5a1cd33179119db817c30315d68b84ac16fda51
TOP_GUN <k02622@naver.com>
2026-09-12 21:50:15 +0900

    [Passed] 합과 평균의 차 (48ms, 16MB)

---
+++ 260912/합과 평균의 차/sub-of-average-and-sum.py
@@ -0,0 +1,11 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+print(a + b + c)
+print((a + b + c) // 3)
+print((a + b + c) - (a + b + c) // 3)
