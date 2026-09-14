1d25b336d9a43a5586b6c761e9f8e8449df75e5b
TOP_GUN <k02622@naver.com>
2026-09-14 22:34:09 +0900

    [Passed] 두 수의 짝홀 여부 (56ms, 16MB)

---
+++ 260914/두 수의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,13 @@
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+if a % 2 == 0:
+    print("even")
+else :
+    print("odd")
+
+if b % 2 == 0:
+    print("even")
+else :
+    print("odd")
