deaacb5baeec785f7cc77fa6d1a6283a33e37468
TOP_GUN <k02622@naver.com>
2026-09-14 22:20:48 +0900

    [Passed] 알파벳에 따른 평가 (57ms, 16MB)

---
+++ 260914/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+# 변수 선언, 입력
+a = input()
+
+# 출력
+if a == 'S':
+    print("Superior")
+elif a == 'A':
+    print("Excellent")
+elif a == 'B':
+    print("Good")
+elif a == 'C':
+    print("Usually")
+elif a == 'D':
+    print("Effort")
+else:
+    print("Failure")
