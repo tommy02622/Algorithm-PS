097c4f0bd981ebd79f1dfc34f9c4d0dbf2ef4d9b
TOP_GUN <k02622@naver.com>
2026-09-14 22:19:41 +0900

    [Passed] 등급 매기기 (50ms, 16MB)

---
+++ 260914/등급 매기기/ranking.py
@@ -0,0 +1,14 @@
+# 변수 선언, 입력
+n = int(input())
+
+# 출력
+if n >= 90:
+    print("A")
+elif n >= 80:
+    print("B")
+elif n >= 70:
+    print("C")
+elif n >= 60:
+    print("D")
+else:
+    print("F")
