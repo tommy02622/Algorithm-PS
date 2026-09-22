e9052a50c5b6351538161ebdea5165ac2c1ae370
TOP_GUN <k02622@naver.com>
2026-09-22 17:11:52 +0900

    [Passed] 수의 계절은 (52ms, 16MB)

--- 260922/수의 계절은/season-of-num.py
+++ 260922/수의 계절은/season-of-num.py
@@ -1,10 +1,12 @@
+# 변수 선언, 입력
 m = int(input())
 
-if  m >= 3 and m <=5:
-    print("Spring")
-elif m >=6 and m<=8:
-    print("Summer")
-elif m>=9 and m<=11:
-    print("Fall")
-else:
+# 출력
+if m >= 12 or m <= 2:
     print("Winter")
+elif m <= 5:
+    print("Spring")
+elif m <= 8:
+    print("Summer")
+else:
+    print("Fall")
